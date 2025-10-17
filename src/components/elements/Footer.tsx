16891c589209eb250279fa299ee83b38f66805a1
54Alexzen <alexzen.develop@gmail.com>
2025-10-17 00:04:30 -0600

    feat (component): add Footer component with developer attribution

---
+++ src/components/elements/Footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <footer className="min-h-[7svh] flex items-center justify-center border-t dark:border-stone-700 border-stone-300">
+      <p className="md:text-sm sm:text-xs text-2xs text-center">
+        Desarrollado por <strong>alexzen.dev</strong> ❤️
+      </p>
+    </footer>
+  );
+};
